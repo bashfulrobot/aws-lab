398f0a2db3fb5b39c668cd4a27b2b9ec2ad7503f
Dustin Krysak <dustin@bashfulrobot.com>
2022-07-28 13:32:02 -0700

    fix: :bug: bug out!
    - Incorrect module git path
    - credentials were not being respected, so they were moved into the provider (with a secrets manager)

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
 provider "aws" {
-  region = var.AWS_REGION
+  region = var.dk_aws_region
+  access_key = var.dk_access_key
+  secret_key = var.dk_secret_key
 
   default_tags {
     tags = {
-        TF-Deploy = var.LAB_NAME
-        Terraform = "true"
+        terraform-source = var.lab_name
+        terraform-deploy = "true"
     }
   }
 }
 
 # Note - I use ssh so I do not have to enter creds over https
+# Because the module is in a subfolder, I needed to target it.
 module "sysdig-cs" {
-    source = "git@github.com:draios/cs-terraform-modules.git"
+    source = "git@github.com:draios/cs-terraform-modules.git//terraform-cs-aws-vpc"
 
-    aws_region = var.AWS_REGION
-    vpc_name = "${var.LAB_NAME}-vpc"
+    aws_region = var.dk_aws_region
+    vpc_name = "${var.lab_name}-vpc"
 
     cloudwatch_log_group_retention = 7
 
--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
-variable "LAB_NAME" {
+variable "lab_name" {
   type = string
   description = "The name of this lab, and also used to prefix tags"
   default = "aws-lab"
 }
 
-variable "AWS_REGION" {
+variable "dk_aws_region" {
   type = string
   description = "The AWS region that all resources/modules will use."
   default = "us-west-2"
+}
+
+variable "dk_access_key" {
+  type = string
+}
+
+variable "dk_secret_key" {
+  type = string
 }
