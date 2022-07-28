b9f230f008bd5ad8a46f3dd8b3a0629168501b64
Dustin Krysak <dustin@bashfulrobot.com>
2022-07-28 13:48:44 -0700

    chore: :art: fixed formating

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = var.dk_aws_region
+  region     = var.dk_aws_region
   access_key = var.dk_access_key
   secret_key = var.dk_secret_key
 
   default_tags {
     tags = {
-        terraform-source = var.lab_name
-        terraform-deploy = "true"
+      terraform-source = var.lab_name
+      terraform-deploy = "true"
     }
   }
 }
@@ -14,12 +14,12 @@ provider "aws" {
 # Note - I use ssh so I do not have to enter creds over https
 # Because the module is in a subfolder, I needed to target it.
 module "sysdig-cs" {
-    source = "git@github.com:draios/cs-terraform-modules.git//terraform-cs-aws-vpc"
+  source = "git@github.com:draios/cs-terraform-modules.git//terraform-cs-aws-vpc"
 
-    aws_region = var.dk_aws_region
-    vpc_name = "${var.lab_name}-vpc"
+  aws_region = var.dk_aws_region
+  vpc_name   = "${var.lab_name}-vpc"
 
-    cloudwatch_log_group_retention = 7
+  cloudwatch_log_group_retention = 7
 
-    nat_instance = true
+  nat_instance = true
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "lab_name" {
-  type = string
+  type        = string
   description = "The name of this lab, and also used to prefix tags"
-  default = "aws-lab"
+  default     = "aws-lab"
 }
 
 variable "dk_aws_region" {
-  type = string
+  type        = string
   description = "The AWS region that all resources/modules will use."
-  default = "us-west-2"
+  default     = "us-west-2"
 }
 
 variable "dk_access_key" {
